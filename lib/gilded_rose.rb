e22f110b4e144ed2236664045953d0b0491d93e7
John Scott <jfscott@memphis.edu>
2024-02-28 09:43:17 -0600

    normal object tests still red.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,11 +5,12 @@ class GildedRose
     @name = name
     @days_remaining = days_remaining
     @quality = quality
+    
   end
 
   def normal_tick
-    @item = Normal Item.new(quality, days_remaining)
-    item.tick
+    @item = Normal.new(quality:, days_remaining:)
+    @item.tick
   end
 
   def brie_tick
@@ -48,12 +49,27 @@ class GildedRose
       return backstage_tick
     end
   end
+  # class Normal
+  #   attr_reader :quality, :days_remaining
+  
+  #   def initialize(quality:, days_remaining:)
+  #     @quality, @days_remaining = quality, days_remaining
+  #   end
+  
+  #   def tick
+  #     @days_remaining -= 1
+  #     return if @quality == 0
+  
+  #     @quality -= 1
+  #     @quality -= 1 if @days_remaining <= 0
+  #   end
+  # end
 end
 
 class Normal
   attr_reader :quality, :days_remaining
 
-  def initialize(quality, days_remaining)
+  def initialize(quality:, days_remaining:)
     @quality, @days_remaining = quality, days_remaining
   end
 
