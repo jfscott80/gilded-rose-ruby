47121984ff9bb8df0ebfad56addd40b8100d9014
John Scott <jfscott@memphis.edu>
2024-03-12 17:18:01 -0500

    specialized classes hash initiated in module

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,63 +1,60 @@
 module GildedRose
 
-  def self.new(name:, quality:, days_remaining:)
-    klass_for(name).new(quality:, days_remaining:)
-  end
 
-  def self.klass_for(name)
-    case name
-    when 'Normal Item'
-      Normal
-    when 'Aged Brie'
-      Brie
-    when 'Sulfuras, Hand of Ragnaros'
-      Sulfuras
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      Backstage
+  class Item
+    attr_reader :quality, :days_remaining
+  
+    def initialize(quality:, days_remaining:)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
     end
   end
-end
-class Item
-  attr_reader :quality, :days_remaining
 
-  def initialize(quality:, days_remaining:)
-    @quality, @days_remaining = quality, days_remaining
+
+  class Normal < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
   end
 
-end
-class Normal < Item
-  def tick
-    @days_remaining -= 1
-    return if @quality == 0
-
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
+  class Backstage < Item
+    def tick
+      @days_remaining -= 1
+      return @quality = 0 if @days_remaining <0
+      return if @quality == 50
+    
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 5
+    
+    end
   end
-end
 
-class Backstage < Item
-   def tick
-    @days_remaining -= 1
-    return @quality = 0 if @days_remaining <0
-    return if @quality == 50
+  class Brie < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+    
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
+    end
+  end
   
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
-  
-  end
-end
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'Normal Item'                               => Normal,
+    'Aged Brie'                                 => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage
+                                                              }
 
-class Brie < Item
-  def tick
-    @days_remaining -= 1
-    return if @quality >= 50
-  
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0 && @quality < 50
-  end
-end
-class Sulfuras < Item
-  def tick
+  def self.new(name:, quality:, days_remaining:)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).
+      new(quality:, days_remaining:)
+
   end
 end
